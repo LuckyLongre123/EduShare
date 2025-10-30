ee26a136505aaf0c27e65c06dd4c1518928fdc79
Lucky Longre <officialluckylongre@gmail.com>
2025-10-30 11:04:56 +0530

    Update sql_query.sql

--- sql_query.sql
+++ sql_query.sql
@@ -22,8 +22,9 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
+CREATE DATABASE IF NOT EXISTS if0_39937351_project_db;
+USE if0_39937351_project_db;
 
---
 -- Table structure for table `colleges`
 --
 
